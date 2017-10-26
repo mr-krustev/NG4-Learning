f996d8fc1266434ed19350d966baa73403e71a12
Martin <mr.krustev1994@gmail.com>
2017-10-26 23:37:18 +0300

    Added hero service and learned about Promises and async request.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,7 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Component, OnInit } from '@angular/core';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -20,13 +9,15 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
 	<ul class="heroes">
-	  <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+	  <li *ngFor="let hero of heroes" 
+  		[class.selected]="hero === selectedHero" 
+  		(click)="onSelect(hero)">
 	    <span class="badge">{{hero.id}}</span> {{hero.name}}
 	  </li>
 	</ul>
 	<hero-detail [hero]="selectedHero"></hero-detail>
     `,
-    styles: [`
+   styles: [`
   .selected {
     background-color: #CFD8DC !important;
     color: white;
@@ -74,17 +65,24 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
-
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+  	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  };
+
+  ngOnInit(): void {
+  	this.getHeroes();
+  };
+
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
   };
---
+++ src/app/hero.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { Heroes } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(Heroes);
+  }
+}

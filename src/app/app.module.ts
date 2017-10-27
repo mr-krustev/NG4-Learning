ed06b14716589bcff1569144192536422de1d7c0
Martin <mr.krustev1994@gmail.com>
2017-10-27 23:10:39 +0300

    Created Heroes.Component to handle the hero visualization. AppCommonent will work as a router/navigator.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
+// Components
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+
+// Services
+import { HeroService } from './hero.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

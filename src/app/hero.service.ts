fcebfca60a5c1d577d406fc6dd065f47aa61d54e
Martin <mr.krustev1994@gmail.com>
2017-10-28 16:54:37 +0300

    Finished the DBServiceMocking. Fixed a bad call to styles.css. Fixed Promise response from HeroService.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { Heroes } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(Heroes);
-  }
+  private heroesUrl = 'api/heroes';
 
-  getHero(id: number): Promise<Hero> {
-  	return this.getHeroes()
-  			.then(heroes => heroes.find(hero => hero.id === id));
-  }
+  constructor(private http: Http) { }
+
+  getHeroes(): Promise<Hero[]> {
+  		return this.http.get(this.heroesUrl)
+             .toPromise()
+             .then(response => response.json() as Hero[])
+             .catch(this.handleError);
+	}
+	 
+	private handleError(error: any): Promise<any> {
+	  console.error('An error occurred', error); // for demo purposes only
+	  return Promise.reject(error.message || error);
+	}
+
+	getHero(id: number): Promise<Hero> {
+		const url = `${this.heroesUrl}/${id}`;
+	    return this.http.get(url)
+		    .toPromise()
+		    .then(response => response.json() as Hero)
+		    .catch(this.handleError);
+	}
 }

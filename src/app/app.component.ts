004de20ec3a72d2e13470803cecbca21b256976b
Martin <mr.krustev1994@gmail.com>
2017-10-28 16:13:51 +0300

    Split HTML/CSS/Code logic to respectiv files. Added Angular-provided CSS to beautify the front-end.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,8 @@ import { Component } from '@angular/core';
  
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-    	<a routerLink="/heroes">Heroes</a>
-    	<a routerLink="/dashboard">Dashboard</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -13,7 +14,14 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+    ) { }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  };
 
   getHeroes(): void {
   	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
